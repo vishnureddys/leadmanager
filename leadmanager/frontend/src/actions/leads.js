23c890b290e5252029c76e53c58c6f8d9cd6e8b6
Vishnu S Reddy <52468309+Vishnu-S-Reddy@users.noreply.github.com>
2020-05-16 14:42:36 +0530

    Added a ADD_LEAD part.

--- leadmanager/frontend/src/actions/leads.js
+++ leadmanager/frontend/src/actions/leads.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-import { GET_LEADS, DELETE_LEADS } from './types';
+import { GET_LEADS, DELETE_LEADS, ADD_LEAD } from './types';
 
 //GET LEADS
 export const getLeads = () => dispatch => {
@@ -14,13 +14,24 @@ export const getLeads = () => dispatch => {
 };
 
 //Delete lead
-export const deleteLeads = () => dispatch => {
+export const deleteLeads = (id) => dispatch => {
     axios
-        .get(`/api/leads/${id}/`)
+        .delete(`/api/leads/${id}/`)
         .then(res => {
             dispatch({
                 type: DELETE_LEADS,
                 payload: id
+            });
+        }).catch(err => console.log(err));
+};
+
+//ADD LEAD
+export const addLead = (lead) => dispatch => {
+    axios.post('/api/leads/', lead)
+        .then(res => {
+            dispatch({
+                type: ADD_LEAD,
+                payload: res.data
             });
         }).catch(err => console.log(err));
 };
--- leadmanager/frontend/src/actions/types.js
+++ leadmanager/frontend/src/actions/types.js
@@ -1,2 +1,3 @@
 export const GET_LEADS = "GET_LEADS";
 export const DELETE_LEADS = "DELETE_LEADS";
+export const ADD_LEAD = "ADD_LEAD";
